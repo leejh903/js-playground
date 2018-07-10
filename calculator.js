123c30fbcf8a11f519ed916484cd7b8fb08569fe
Brad Lee <brad903@naver.com>
2018-07-10 18:39:55 +0900

    한 줄로 입력 받아 계산기 처리하기
    기존 코드에서 객체화함

--- calculator.js
+++ calculator.js
@@ -1,37 +1,40 @@
-var input_value;
-var input_array = [];
-var input_length;
-var result;
+var input = {};
+input.input_value;
+input.input_array = [];
+input.input_length;
+input.input_val = document.getElementById('input_num');
 
-var cal_num = function() {
-	var input_val = document.getElementById('input_num');
-    input_value = input_val.value;
-    input_array = input_value.split(" ");
-    input_length = input_array.length;
-    result = Number(input_array[0]);
+var cal = {};
+cal.result;
+cal.cal_num = function() {
+    input.input_value = input.input_val.value;
+    input.input_array = input.input_value.split(" ");
+    input.input_length = input.input_array.length;
+    this.result = Number(input.input_array[0]);
 
     main();
 };
 
-var calculate = function(input_array, input_length) {
+cal.calculate = function(input_array, input_length) {
     for(var i = 1; i < input_length; i=i+2){
         var obj = Number(input_array[i+1]);
         switch(input_array[i]){
-            case '+': result = result + obj; break;
-            case '-': result = result - obj; break;
-            case '*': result = result * obj; break;
-            case '/': result = result / obj; break;
+            case '+': this.result = this.result + obj; break;
+            case '-': this.result = this.result - obj; break;
+            case '*': this.result = this.result * obj; break;
+            case '/': this.result = this.result / obj; break;
         }
     }
 }
 
-var print = function(){
-    var output_box = document.getElementById('output_num');
-    output_box.value = result;
+var output = {};
+output.output_box = document.getElementById('output_num');
+output.print = function(){
+    output.output_box.value = cal.result;
 }
 
 var main = function(){
-    calculate(input_array, input_length);
-    print();
+    cal.calculate(input.input_array, input.input_length);
+    output.print();
 }
 
