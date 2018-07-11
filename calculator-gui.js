281390924b78c2de61b94e9a7ab0b04cd24a0941
Brad Lee <brad903@naver.com>
2018-07-11 11:26:34 +0900

    Revert "GUI 계산기 만들기1"
    This reverts commit 5c37fac841001fb5405d88eaa9027c10f6741ca5.

--- calculator-gui.js
+++
@@ -1,9 +0,0 @@
-    var click_num = function(event){
-        console.log(event.target.innerHTML);
-        console.log("click_num");
-    }
-
-    var click_other = function(event){
-        console.log(event.target.innerHTML);
-        console.log("click_other");
-    }
