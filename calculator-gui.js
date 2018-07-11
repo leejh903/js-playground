9b3ed015a7215554ac0e8c1fe4d8b69e9550dca5
Brad Lee <brad903@naver.com>
2018-07-11 17:36:47 +0900

    GUI 계산기 만들기2
    요구사항
     - 간단 css 적용
     - 입력받은 문자들을 하나의 문자열로 합친다.
     - 백스페이스 키가 동작하도록 한다

--- calculator-gui.js
+++ calculator-gui.js
@@ -1,6 +1,27 @@
+var input = {};
+input.array = [];
+
+var output = {};
+output.display = document.getElementById('output');
+
 var click_num = function(event){
-    console.log(event.target.innerHTML);
-    console.log("click_num");
+    var target = event.target.innerHTML;
+    switch(target){
+        // 사칙연산의 경우
+        case '+': case '-': case '*': case '/':
+        target = ' ' + target + ' ';  // 공백 단위로 받아주기 위해 일부러 삽입
+        input.array.push(target);
+        break;
+        // 지우기 경우
+        case 'BS':
+        input.array.pop();
+        break;
+        // 일반 수의 경우
+        default:
+        input.array.push(target);
+    }
+    if(input.array.length === 0) output.display.innerHTML = "empty";
+    else output.display.innerHTML = input.array.join("");
 }
 
 var click_other = function(event){
