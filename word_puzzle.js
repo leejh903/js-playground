f9d95acc6066ec1dce0c0af6910416f909e8fc64
Brad Lee <brad903@naver.com>
2018-07-13 18:00:51 +0900

    레이아웃 요구 조건에 맞게 수정
     - 좀더 적절한 태그로 수정
     - css 추가
     - 적절한 id, 핸들러 이름으로 수정
     - JS 통해 버튼 생성 코드 추가

--- word_puzzle.js
+++ word_puzzle.js
@@ -1,9 +1,15 @@
-var WordAdd = function(){
-    console.log("wordadd");
+var str = document.getElementById('word1').innerHTML;
+var word2 = document.getElementById('word2');
+
+for(var i=0; i<str.length; i++){
+    var btn = document.createElement('button');
+    btn.innerHTML = str[i];
+    word2.appendChild(btn);
 }
-var WordTurn = function(){
-    console.log("wordturn");
+
+var swap = function(event){
+    console.log("swap");
 }
-var WordPush = function(){
-    console.log("wordpush");
+var shift = function(){
+    console.log("shift");
 }
