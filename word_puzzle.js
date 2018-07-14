27a98fcfe07f6776ec09738c9dd1067ad4fb406d
Brad Lee <brad903@naver.com>
2018-07-14 21:55:13 +0900

    객체를 이용한 리팩토링
    객체 속성과 메서드 형태로 리팩토링

--- word_puzzle.js
+++ word_puzzle.js
@@ -1,76 +1,77 @@
+game = {};
+game.answer;
+game.answer_array;
 var word1 = document.getElementById('word1');
 var word2 = document.getElementById('word2');
-var str;
-var str_array;
 var word = ['apple','pineapple','puppy','javascript','iloveyou','bradlee','davidchoi','whiteboard','internet','ipadstand'];
 
-var select_word = function(){
+game.select_word = function(){
     var num = Math.floor(Math.random()*word.length);
     var selected_word = word[num];
     word1.innerHTML = selected_word;
-    str = word1.innerHTML;
-    str_array = str.split("");
-    check_str();
-}
+    this.answer = word1.innerHTML;
+    this.answer_array = this.answer.split("");
+    game.check_str();
+};
 
-var shuffle = function(){
+game.shuffle = function(){
     var toggle = Math.floor(Math.random() * 2) === 1;
-    if(toggle) swap();
+    if(toggle) game.swap();
 
-    var n = Math.floor(Math.random() * str.length)
+    var n = Math.floor(Math.random() * this.answer.length)
 
-    for(var i=0; i<n; i++) right_shift();
-}
+    for(var i=0; i<n; i++) game.right_shift();
+};
 
-var create_btn = function(){
-    for(var i=0; i<str.length; i++){
+game.create_btn = function(){
+    for(var i=0; i<this.answer.length; i++){
         var btn = document.createElement('button');
-        btn.innerHTML = str[i];
+        btn.innerHTML = this.answer[i];
         word2.appendChild(btn);
     }
 }
 
-var change_btn = function(){
+game.change_btn = function(){
     var child = document.getElementById("word2").firstChild;
-    for(var i=0; i<str.length; i++){
-        child.innerHTML = str[i];
+    for(var i=0; i<this.answer.length; i++){
+        child.innerHTML = this.answer[i];
         child = child.nextSibling;
     }
 }
 
-var swap = function(event){
+game.swap = function(){
     var temp_array = [];
     var temp;
-    for(var i=str.length-1; i>=0; i--){
-        temp = str_array.pop();
+    for(var i=this.answer.length-1; i>=0; i--){
+        temp = this.answer_array.pop();
         temp_array.push(temp);
     }
-    str_array = temp_array;
-    str = str_array.join("");
+    this.answer_array = temp_array;
+    this.answer = this.answer_array.join("");
     
-    change_btn();
-    check_str();
+    game.change_btn();
+    game.check_str();
 }
 
-var left_shift = function(){
-    var temp = str_array.shift();
-    str_array.push(temp);
-    str = str_array.join("");
-    change_btn();
-    check_str();
+game.left_shift = function(){
+    var temp = this.answer_array.shift();
+    this.answer_array.push(temp);
+    this.answer = this.answer_array.join("");
+    game.change_btn();
+    game.check_str();
 }
-var right_shift = function(){
-    var temp = str_array.pop();
-    str_array.unshift(temp);
-    str = str_array.join("");
-    change_btn();
-    check_str();
+game.right_shift = function(){
+    var temp = this.answer_array.pop();
+    this.answer_array.unshift(temp);
+    this.answer = this.answer_array.join("");
+    game.change_btn();
+    game.check_str();
 }
 
-var check_str = function(){
+game.check_str = function(){
     var target = document.getElementById('word1').innerHTML;
     var display_check = document.getElementById('check');
-    if(str === target){
+    if(this.answer === target){
         display_check.innerHTML = "일치합니다";
     }else{
         display_check.innerHTML = "일치하지 않습니다";
@@ -78,9 +79,9 @@ var check_str = function(){
 }
 
 var main = function(){
-    select_word();
-    create_btn();
-    shuffle();
+    game.select_word();
+    game.create_btn();
+    game.shuffle();
 }
 
 main();
