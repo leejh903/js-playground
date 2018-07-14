38d237b24e3990be894c520c29bdc8da38c3f33d
Brad Lee <brad903@naver.com>
2018-07-14 22:51:52 +0900

    퍼즐 구현4: 진행상황 처리
    요구사항
     - 한 문제를 맞추면 다음 문제를 출제한다.
     - 한 문제를 맞출 때마다 상단에 O를 하나씩 추가한다.
     - 세 문제를 맞추면 Thank you for playing! 문구를 표시한다.

--- word_puzzle.js
+++ word_puzzle.js
@@ -1,6 +1,9 @@
 game = {};
 game.answer;
 game.answer_array;
+game.progress = "";
+game.progress_num = 0;
+var progress = document.getElementById('progress');
 var word1 = document.getElementById('word1');
 var word2 = document.getElementById('word2');
 var word = ['apple','pineapple','puppy','javascript','iloveyou','bradlee','davidchoi','whiteboard','internet','ipadstand'];
@@ -11,7 +14,6 @@ game.select_word = function(){
     word1.innerHTML = selected_word;
     this.answer = word1.innerHTML;
     this.answer_array = this.answer.split("");
-    game.check_str();
 };
 
 game.shuffle = function(){
@@ -29,7 +31,13 @@ game.create_btn = function(){
         btn.innerHTML = this.answer[i];
         word2.appendChild(btn);
     }
+};
+
+game.remove_btn = function(){
+for(var i=0; i<this.answer.length; i++){
+    word2.removeChild(word2.childNodes[0]);
 }
+};
 
 game.change_btn = function(){
     var child = document.getElementById("word2").firstChild;
@@ -37,7 +45,7 @@ game.change_btn = function(){
         child.innerHTML = this.answer[i];
         child = child.nextSibling;
     }
-}
+};
 
 game.swap = function(){
     var temp_array = [];
@@ -51,7 +59,7 @@ game.swap = function(){
     
     game.change_btn();
     game.check_str();
-}
+};
 
 game.left_shift = function(){
     var temp = this.answer_array.shift();
@@ -59,29 +67,43 @@ game.left_shift = function(){
     this.answer = this.answer_array.join("");
     game.change_btn();
     game.check_str();
-}
+};
 game.right_shift = function(){
     var temp = this.answer_array.pop();
     this.answer_array.unshift(temp);
     this.answer = this.answer_array.join("");
     game.change_btn();
     game.check_str();
-}
+};
 
 game.check_str = function(){
     var target = document.getElementById('word1').innerHTML;
     var display_check = document.getElementById('check');
     if(this.answer === target){
         display_check.innerHTML = "일치합니다";
+        game.game_progress();
     }else{
         display_check.innerHTML = "일치하지 않습니다";
     }
+};
+
+game.game_progress = function(){
+    game.progress_num++;
+    game.progress += "O "
+    progress.innerHTML = game.progress;
+    if(game.progress_num < 3){
+        game.remove_btn();
+        NewGame();
+    } else {
+        game.progress = "Thank you for playing!"
+        progress.innerHTML = game.progress;
+    }
 }
 
-var main = function(){
+var NewGame = function(){
     game.select_word();
     game.create_btn();
     game.shuffle();
 }
 
-main();
+NewGame();
