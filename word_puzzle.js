3a3de013f3bb4ebd453336f56c8e1bf92eea7c6a
Brad Lee <brad903@naver.com>
2018-07-14 17:41:56 +0900

    단어 퍼즐 구현하기2 : 버튼 기능
    요구사항
     - 밀어내기 버튼을 누를 경우 문자열이 오른쪽으로 한 칸씩 이동한다. 예를 들어 “HELLO” 상태에서 누르면 “OHELL”이 된다.
     - 같은 방식으로 왼쪽 뒤집기도 구현한다.
     - 뒤집기 버튼을 누를 경우 문자열이 뒤집힌다. 예를 들어 “HELLO” 상태에서 누르면 “OLLEH”가 된다.
     - 첫 줄의 문자열과 버튼의 문자열이 일치할 경우 화면에 “일치합니다.” 그렇지 않을 경우 “일치하지 않습니다” 를 표시한다.

--- word_puzzle.js
+++ word_puzzle.js
@@ -1,15 +1,57 @@
 var str = document.getElementById('word1').innerHTML;
+var str_array = str.split("");
 var word2 = document.getElementById('word2');
 
-for(var i=0; i<str.length; i++){
-    var btn = document.createElement('button');
-    btn.innerHTML = str[i];
-    word2.appendChild(btn);
+var create_btn = function(){
+    for(var i=0; i<str.length; i++){
+        var btn = document.createElement('button');
+        btn.innerHTML = str[i];
+        word2.appendChild(btn);
+    }
+}
+
+var change_btn = function(){
+    var child = document.getElementById("word2").firstChild;
+    for(var i=0; i<str.length; i++){
+        child.innerHTML = str[i];
+        child = child.nextSibling;
+    }
 }
 
 var swap = function(event){
-    console.log("swap");
+    var temp_array = [];
+    var temp;
+    for(var i=str.length-1; i>=0; i--){
+        temp = str_array.pop();
+        temp_array.push(temp);
+    }
+    str_array = temp_array;
+    str = str_array.join("");
+    
+    change_btn();
+    check_str();
 }
+
 var shift = function(){
-    console.log("shift");
+    var temp = str_array.pop();
+    str_array.unshift(temp);
+    str = str_array.join("");
+    change_btn();
+    check_str();
 }
+
+var check_str = function(){
+    var target = document.getElementById('word1').innerHTML;
+    var display_check = document.getElementById('check');
+    if(str === target){
+        display_check.innerHTML = "일치합니다";
+    }else{
+        display_check.innerHTML = "일치하지 않습니다";
+    }
+}
+
+var main = function(){
+    create_btn();
+}
+
+main();
